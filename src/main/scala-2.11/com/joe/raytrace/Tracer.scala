119903dbef70de3934a7fcea75af5d3204dcdcd9
Joe Carroll <josephcarroll1@gmail.com>
2015-11-15 13:55:06 -0500

    Added simple shading support.

--- src/main/scala-2.11/com/joe/raytrace/Tracer.scala
+++ src/main/scala-2.11/com/joe/raytrace/Tracer.scala
@@ -18,12 +18,18 @@ object Tracer {
           val normal = (intersection.point - intersection.obj.position).normalize
           val lightRay = (light.position - normal).normalize
           val intensity = Math.max(0, lightRay.dot(normal))
-          light.colour * intensity
+
+          val inShade = if (blocked(scene, Ray(intersection.point + normal, lightRay))) 0.0 else 1.0
+          light.colour * inShade * intensity
         }
         intersection.obj.colour * (lightInput.foldLeft(Vector(0.0, 0.0, 0.0))(_ + _) + scene.ambientLight).cap(1.0)
       case None =>
         scene.backgroundColour
     }
+  }
+
+  private def blocked(scene: Scene, ray: Ray): Boolean = {
+    scene.spheres.exists(_.intersects(ray).isDefined)
   }
 
   case class Ray(origin: Vector, direction: Vector) {
