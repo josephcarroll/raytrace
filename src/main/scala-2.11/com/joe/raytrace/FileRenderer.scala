135e818fde34b3454d6243a896175d56f00f4edb
Joe Carroll <josephcarroll1@gmail.com>
2015-11-15 12:55:56 -0500

    Added green pixel if colour is ever too bright.

--- src/main/scala-2.11/com/joe/raytrace/FileRenderer.scala
+++ src/main/scala-2.11/com/joe/raytrace/FileRenderer.scala
@@ -8,10 +8,15 @@ object FileRenderer {
 
   def renderToFile(width: Int, height: Int, pixels: Array[Vector]): Unit = {
     val bytes = pixels.flatMap { colour =>
-      val r = (255 * colour.x).toByte
-      val g = (255 * colour.y).toByte
-      val b = (255 * colour.z).toByte
-      Seq(r, g, b)
+      if (colour.x > 1.0 || colour.y > 1.0 || colour.z > 1.0) {
+        // This is to point out errors where our final pixel is too bright!
+        Seq(0.toByte, 255.toByte, 0.toByte)
+      } else {
+        val r = (255 * colour.x).toByte
+        val g = (255 * colour.y).toByte
+        val b = (255 * colour.z).toByte
+        Seq(r, g, b)
+      }
     }
 
     val f = new File("/Users/Joe/Desktop/result.ppm")
