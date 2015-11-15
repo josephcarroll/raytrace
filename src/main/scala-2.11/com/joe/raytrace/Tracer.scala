7dc08ff5229641d4231885ca0c5a5a80ae0ec8dc
Joe Carroll <josephcarroll1@gmail.com>
2015-11-14 19:09:09 -0500

    Added initial shading implementation.

--- src/main/scala-2.11/com/joe/raytrace/Tracer.scala
+++ src/main/scala-2.11/com/joe/raytrace/Tracer.scala
@@ -6,31 +6,30 @@ import com.joe.raytrace.Tracer._
 
 object RayTracer extends App {
 
-  val closeRed = Sphere(Vector(0.0, 0.0, -20.0), 2.0, Vector(1.0, 0.0, 0.0))
-  val farYellow = Sphere(Vector(2.0, 2.0, -40.0), 6.0, Vector(1.0, 1.0, 0.0))
-  val objects = Seq(closeRed, farYellow)
+  val lightSphere = Sphere(Vector(0.0, 0.0, -20.0), 1.0, Vector(1.0, 1.0, 1.0))
+  val red = Sphere(Vector(2.0, 2.0, -20.0), 1.0, Vector(1.0, 0.0, 0.0))
+  val yellow = Sphere(Vector(-2.0, -2.0, -20.0), 1.0, Vector(1.0, 1.0, 0.0))
+  val objects = Seq(lightSphere, red, yellow)
 
-  val camera = Camera(Ray(Vector(0.0, 0.0, 0.0), Vector(0.0, 0.0, -1.0)), 600, 600)
+  val light = Light(Vector(10.0, 10.0, 5.0), Vector(0.9, 1.0, 0.9))
+  val lights = Seq(light)
+
+  val camera = Camera(Ray(Vector(0.0, 0.0, 5.0), Vector(0.0, 0.0, -1.0)), 600, 600)
 
   val invWidth = 1.0 / camera.width
   val invHeight = 1.0 / camera.height
-  val angle = Math.tan(Math.PI * 0.5 * camera.fieldOfView / 180.0)
+  val angle = Math.tan(Math.PI * camera.origin.z * camera.fieldOfView / 180.0)
 
   val image = for (x <- 0 until camera.width; y <- 0 until camera.height) yield {
 
-    val xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * camera.aspectRatio
-    val yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle
+    val xx = (2 * ((x + camera.origin.z) * invWidth) - 1) * angle * camera.aspectRatio
+    val yy = (1 - 2 * ((y + camera.origin.z) * invHeight)) * angle
     val rayDirection = Vector(xx, yy, camera.direction.z).normalize
     traceRay(Ray(camera.origin, rayDirection))
 
   }
 
-  val colours = image.map {
-    case Some(intersection) => intersection.obj.colour
-    case None               => Vector(0.0, 0.0, 0.0)
-  }
-
-  val bytes = colours.flatMap { colour =>
+  val bytes = image.flatMap { colour =>
     val r = (255 * colour.x).toByte
     val g = (255 * colour.y).toByte
     val b = (255 * colour.z).toByte
@@ -49,10 +48,19 @@ object RayTracer extends App {
     (to - from.point).length
   }
 
-  def traceRay(ray: Ray): Option[Intersection] = {
+  def traceRay(ray: Ray): Vector = {
     val intersections = objects.flatMap(_.intersects(ray))
     val intersectionByDistance = intersections.sortBy(distanceTo(ray.origin))
-    intersectionByDistance.headOption
+
+    intersectionByDistance.headOption match {
+      case Some(intersection) =>
+        val normal = (intersection.point - intersection.obj.center).normalize
+        val lightRay = (light.location - normal).normalize
+        val colour = Math.max(0, lightRay.dot(normal))
+        intersection.obj.colour * light.colour * colour
+      case None =>
+        Vector(0.0, 0.0, 0.0)
+    }
   }
 
 }
@@ -66,6 +74,7 @@ object Tracer {
     def +(that: Vector): Vector = Vector(this.x + that.x, this.y + that.y, this.z + that.z)
     def -(that: Vector): Vector = Vector(this.x - that.x, this.y - that.y, this.z - that.z)
     def *(that: Vector): Vector = Vector(this.x * that.x, this.y * that.y, this.z * that.z)
+    def *(constant: T): Vector = Vector(this.x * constant, this.y * constant, this.z * constant)
 
     def negate: Vector = Vector(-this.x, -this.y, -this.z)
     def lengthSquared: T = (x * x) + (y * y) + (z * z)
@@ -83,28 +92,40 @@ object Tracer {
 
   }
 
-  case class Ray(origin: Vector, direction: Vector)
+  case class Ray(origin: Vector, direction: Vector) {
+    def pointAt(t: T): Vector = origin + (direction * t)
+  }
 
   case class Intersection(point: Vector, obj: Sphere)
+
+  case class Light(location: Vector, colour: Vector)
 
   case class Sphere(center: Vector, radius: T, colour: Vector) {
     val radiusSquared = radius * radius
     def intersects(ray: Ray): Option[Intersection] = {
-      val l = center - ray.origin
-      val tca = l.dot(ray.direction)
-      if (tca < 0.0) {
-        None
-      } else {
-        val d2 = l.dot(l) - tca * tca
-        if (d2 > radiusSquared) {
-          None
-        } else {
-          // val thc = Math.sqrt(radiusSquared - d2)
-          // t0 = tca - thc
-          // t1 = tca + thc
-          Some(Intersection(Vector(0.0, 0.0, 0.0), this))
-        }
+      val L = center - ray.origin
+      val tca = L.dot(ray.direction)
+      if (tca < 0) return None
+      val d2 = L.dot(L) - (tca * tca)
+      if (d2 > radiusSquared) return None
+      val thc = Math.sqrt(radiusSquared - d2)
+      var t0 = tca - thc
+      var t1 = tca + thc
+
+      if (t0 > t1) {
+        val tmp = t0
+        t0 = t1
+        t1 = tmp
       }
+
+      if (t0 < 0) {
+        t0 = t1 // if t0 is negative, let's use t1 instead
+        if (t0 < 0) return None // both t0 and t1 are negative
+      }
+
+      val t = t0
+
+      return Some(Intersection(ray.pointAt(t), this))
     }
   }
 
