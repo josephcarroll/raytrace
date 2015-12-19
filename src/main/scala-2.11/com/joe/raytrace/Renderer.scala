1883b74e6bd49b883ea32c5153526e6f73b6df75
Joe Carroll <josephcarroll1@gmail.com>
2015-12-19 12:48:42 -0500

    Restored non square rendering.

--- src/main/scala-2.11/com/joe/raytrace/Renderer.scala
+++ src/main/scala-2.11/com/joe/raytrace/Renderer.scala
@@ -9,26 +9,20 @@ object Renderer {
     val width           = pixelWidth * antialiasing
     val height          = pixelHeight * antialiasing
     val samplesPerPixel = Math.pow(antialiasing, 2)
-    val aspectRatio     = width / height
     val camera          = scene.camera
-
-    val imageWidth      = 1.0
-    val imageWidthX2    = imageWidth * 2.0
-    val imageHeight     = 1.0 * aspectRatio
-    val imageHeightX2   = imageWidth * 2.0
 
     def trace = traceRay(scene) _
 
     val image = Array.fill(width * height)(Vector.Zero)
     for (y <- (0 until height).par; x <- (0 until width).par) {
-      val xOffset = imageWidth - (imageWidthX2 * (x / width.toDouble))
-      val yOffset = imageHeight - (imageHeightX2 * (y / height.toDouble))
+      val xOffset = 1.0 - (2.0 * (x / width.toDouble))
+      val yOffset = 1.0 - (2.0 * (y / height.toDouble))
       val direction = camera.direction + Vector(yOffset, xOffset, 0.0)
       image(x + (width * y)) = trace(Ray(camera.origin, direction.normalize))
     }
 
     val pixels = Array.fill(pixelWidth * pixelHeight)(Vector.Zero)
-    for (y <- (0 until height).par; x <- (0 until width).par) {
+    for (y <- 0 until height; x <- 0 until width) {
       val realIndex = x + (width * y)
       val index = (x / antialiasing) + (pixelWidth * (y / antialiasing))
       pixels(index) += image(realIndex)
